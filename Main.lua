c12cda6f9053760e756fddc994c77669f6ec5c8d
kyawlinaung741 <55587156+kyawlinaung741@users.noreply.github.com>
2021-09-30 01:33:15 +0800

    update Main.lua

--- Main.lua
+++ Main.lua
@@ -706,7 +706,7 @@ function addtostr(add,amount)
 end
 
 function getpatch()
-  API = gg.makeRequest('https://raw.githubusercontent.com/Kelrit402/TGC_Sky_is_dead/main/Sharelocate.lua').content
+  API = gg.makeRequest('https://raw.githubusercontent.com/kyawlinaung741/TGC_Sky_is_dead/main/Sharelocate.lua').content
   if not API then
     gg.toast('patch failed')
   else
@@ -1103,7 +1103,7 @@ mm = {}
  if gg.getResultsCount() > 3 then
  nn = gg.getResults(5)[4].address
  gg.clearResults()
- setstr(nn,27,'by ExMachina')
+ setstr(nn,27,'စိတ်၏သခင်')
  end
  nn = 0
  gg.clearResults()
